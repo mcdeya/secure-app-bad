0bff1790ff519dbf2b59c3e57aaf53be2c68f4ab
mcdeya <andreanicole.tekiko@my.jru.edu>
2026-04-19 06:26:57 +0800

    bad

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,1 +1,18 @@
+import json
+from app.main import app
 
+def test_home():
+    client = app.test_client()
+    response = client.get("/")
+    assert response.status_code == 200
+
+def test_add_numbers():
+    client = app.test_client()
+    response = client.post("/add",
+        data=json.dumps({"a": 5}),
+        content_type="application/json"
+    )
+
+    data = json.loads(response.data)
+
+    assert data["result"] == 999
